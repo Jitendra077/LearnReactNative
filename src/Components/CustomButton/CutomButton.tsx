9e4068eb2216a26660768ab0f1679616f538aad0
kishore kumar <kikumar@dminc.com>
2023-07-05 16:34:07 +0530

    Implemented custom button

---
+++ src/Components/CustomButton/CutomButton.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ViewStyle,
+  TextStyle,
+} from 'react-native';
+
+interface CustomButtonProps {
+  title: string;
+  onPress: () => void;
+  buttonStyle?: ViewStyle;
+  textStyle?: TextStyle;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  onPress,
+  buttonStyle,
+  textStyle,
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.button, buttonStyle]}>
+      <Text style={[styles.buttonText, textStyle]}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'green',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomButton;
--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -1,35 +1,45 @@
-import { Button, View } from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import CustomLabel from '../../Components/CustomLabel/CustomLabel';
 import Card from '../../Components/Card/Card';
 import CustomInput from '../../Components/CustomInput/CustomInput';
+import CustomButton from '../../Components/CustomButton/CutomButton';
 
 const Login = () => {
-    return (
-        <Card customCardStyle={{ flex: 1 }}>
-            <View style={{ justifyContent: 'center', marginTop: 85 }}>
-                <CustomLabel lableName="Learn React Native" customStyleLabel={{ color: 'darkgreen', fontSize: 30, fontWeight: 'bold', }} />
-            </View>
-            <View>
-                <CustomLabel lableName="Login" customlabelContainerStyle={{ marginTop: 76 }} />
-            </View>
-            <View>
-                <CustomInput placeholder='Email / Phone number' />
-                <CustomInput placeholder='Password' />
-                <View>
-                    <CustomLabel lableName='Forgot Your Password?'
-                        customStyleLabel={{ fontSize: 9, color: 'green' }}
-                        customlabelContainerStyle={{ alignSelf: 'flex-end', marginTop: 12 }} />
-                </View>
-            </View>
-            <View style={{ marginVertical: 140 }}>
-                <Button title='Login' color={'green'} onPress={{
-                    //TODO:
-                }} />
-
-            </View>
-        </Card>
-    )
-}
+  return (
+    <Card customCardStyle={{flex: 1}}>
+      <View style={{justifyContent: 'center', marginTop: 85}}>
+        <CustomLabel
+          lableName="Learn React Native"
+          customStyleLabel={{
+            color: 'darkgreen',
+            fontSize: 30,
+            fontWeight: 'bold',
+          }}
+        />
+      </View>
+      <View>
+        <CustomLabel
+          lableName="Login"
+          customlabelContainerStyle={{marginTop: 76}}
+        />
+      </View>
+      <View>
+        <CustomInput placeholder="Email / Phone number" />
+        <CustomInput placeholder="Password" />
+        <View>
+          <CustomLabel
+            lableName="Forgot Your Password?"
+            customStyleLabel={{fontSize: 9, color: 'green'}}
+            customlabelContainerStyle={{alignSelf: 'flex-end', marginTop: 12}}
+          />
+        </View>
+      </View>
+      <View style={{marginTop: 120}}>
+        <CustomButton title="Login" />
+      </View>
+    </Card>
+  );
+};
 
 export default Login;
