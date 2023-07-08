fda32dc7b8d8dbf1d8058187cba3a7e63ed6784d
Jitendra Singh <jitsingh@dminc.com>
2023-07-08 15:10:29 +0530

    Create Colors.ts

---
+++ src/Common/Colors.ts
@@ -0,0 +1,49 @@
+export const Colors = {
+    primary: '#424242',
+    white: '#FFFFFF',
+    silver: '#D11E00',
+    yellow: '#FCE300',
+    orange: '#E74C3C',
+    itemSeparator: '#F4F4F4',
+
+    //primary palette
+    primaryCorporate01: '#FCE300',
+    primaryCorporate02: '#1D1D1B',
+    primaryCorporate03: '#424242',
+    primaryCorporateHover01: '#FCF297',
+    primaryCorporateHover02: '#999999',
+
+    //secondary palette
+    secondaryBlack: '#000000',
+    secondarySilver: '#CCCCCC',
+    secondaryNickel: '#666666',
+    secondarySnow: '#F3F4F9',
+    secondaryMoon: '#999999',
+    secondaryWhite: '#FFFFFF',
+
+    //semantic palette
+    semanticfailure: '#E74C3C',
+    semanticFailureDark: '#D11E00',
+    semanticFailureLight: '#FADBD8',
+
+    semanticSuccess: '#2ECC71',
+    semanticSuccessDark: '#03A648',
+    semanticSuccessLight: '#D5F5E3',
+
+    semanticWarning: '#F1C40F',
+    semanticWarningDark: '#C09A01',
+    semanticWarningLight: '#FCF3CF',
+
+    semanticSystem: '#2287FE',
+    semanticSystemDark: '#004E9A',
+    semanticSystemLight: '#D3E7FF',
+
+    //shadow
+    shadowCTA: '#00000021',
+    shadowSwitchOn: '#0000004d',
+
+    //navigation-drawer
+    drawerOverlay: '#2C2C2C',
+};
+
+export default Colors;
